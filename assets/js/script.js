b0c76e0ef848abdbfb2a7b1d26541a245ac6ddd3
Mary Dillon <mary.dillon09@gmail.com>
2022-07-01 16:30:10 -0700

    moves time blocks into jquery from hard coded html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var currentDate = $("#currentDay")
 var container = $(".container")
 var timeOfDay = $(".time-block")
+var scheduleInput = $('.hour')
+var saveBut = $('.saveBtn')
+var rows = $('.row')
 
 
 
@@ -8,14 +11,24 @@ var today = moment().format("dddd, MMMM  Do")
 currentDate.text(today)
 
 
-//fill in time block
-times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+//create time block using jquery instead of hard coding into html
 
-for(i=0; i<times.length; i++){
-    var newRow = $('<div>')
-    newRow.addClass = $('.row')
-    timeOfDay.text(times)
+function printTime(){
+    var times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+
+    for(i=0; i<times.length; i++){
+        var newRow = $('<div>').addClass('row')
+        var newTime = $('<div>').addClass('time-block col-md-2').text(times[i])
+        container.append(newRow)
+        newRow.append(newTime)
+    }
+    
 }
+
+printTime()
+
+
+
 
 
 
